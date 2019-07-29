1c7369f9ded607fd1bbbd25dbb4431ea0d2c1e6f
bpkapkar <49440262+bpkapkar@users.noreply.github.com>
2019-07-29 16:56:24 +0530

    Update Filemover.py

--- Filemover.py
+++ Filemover.py
@@ -1,1 +1,20 @@
-print('hello')
+import os
+import shutil
+import pandas as pd
+data = pd.read_csv('filename.csv',header=None)
+print("Developed by Bhushan Kapkar 9028434644")
+b = list(data.iloc[:,0])
+mydir = input("Source path: ")
+destination = input("Destination Path: ")
+print("Processing")
+for (path , folder, files) in os.walk(mydir):
+    for file in files:
+        for i in b:
+            if i== file:
+                path_file = os.path.join(path,file)
+                shutil.copy(path_file,destination)
+            else:
+                continue
+print("Task Completed")
+
+
